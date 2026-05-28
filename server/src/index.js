c5f5593a839ebf2193d498703c6a1eb5adf1e1ba
Innocent Koge <innocentkaruru16@gmail.com>
2026-05-28 14:31:19 +0300

    fix: update server index

--- server/src/index.js
+++ server/src/index.js
@@ -55,6 +55,12 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 /**
  * =========================
